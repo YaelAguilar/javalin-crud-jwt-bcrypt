95eb144de27cec4add24dcd31402f6bc3fdab313
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-07-11 01:24:24 -0600

    [ADD] dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,35 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+     // --- Framework Web y Logging ---
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+
+    // --- Base de Datos ---
+    // Driver JDBC para la conexión con MySQL.
+    implementation("mysql:mysql-connector-java:8.4.0")
+    // Pool de conexiones de alto rendimiento (HikariCP).
+    implementation("com.zaxxer:HikariCP:5.1.0")
+
+    // --- Seguridad ---
+    // Para hashear y verificar contraseñas con el algoritmo BCrypt.
+    implementation("at.favre.lib:bcrypt:0.10.2")
+    // Para crear y verificar JSON Web Tokens (JWT).
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+    // --- Manejo de JSON (Serialización/Deserialización) ---
+    // Convierte objetos Java (DTOs) a JSON y viceversa.
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+
+     // --- Variables de Entorno ---
+    // Carga las credenciales y configuraciones desde un archivo .env.
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
+
+    // --- Dependencias de Pruebas ---
+    // Framework para pruebas unitarias.
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
